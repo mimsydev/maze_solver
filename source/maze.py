fb862d53519ace9b3a3c3ebb5a3b3a47091a03a5
Johnathan Rhoades <jrhoades7989@gmail.com>
2024-07-06 17:20:38 -0400

    working on maze class

--- source/maze.py
+++ source/maze.py
@@ -2,17 +2,23 @@ from .window import Window
 from .cell import Cell
 from dataclasses import dataclass
 
+@dataclass
 class Maze:
-    def __init__(self) -> None:
-        self.x1: int
-        self.y1: int
-        self.row_count: int
-        self.col_coint: int
-        self.cell_size_x: int
-        self.cell_size_y: int
-        self.win: Window
+    x1: int
+    y1: int
+    row_count: int
+    col_count: int
+    cell_size_x: int
+    cell_size_y: int
+    win: Window
+    _cells: list[list[Cell]]
+
+    def __post_init__(self) -> None:
+        self._create_cells()
 
     def _create_cells(self) -> None:
-        raise NotImplementedError
-
-
+        self._cells = [[ Cell(x1+(j*self.cell_size_x),
+                     y1+(j*self.cell_size_y),
+                     x1+((j+1)*self.cell_size_x),
+                     y1+((j+1)*self.cell_size_y),
+                     self.win) for j in range(col_count) ] for i in range(row_count)]
