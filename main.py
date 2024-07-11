0f43cfb0c00faf4874de6ed5e5b6433c3afd7839
Johnathan Rhoades <jrhoades7989@gmail.com>
2024-07-11 08:13:57 -0400

    Drew first cell

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from source.window import Window
 from source.drawing import Line, Point
+from source.maze import Maze
 
 if __name__ == "__main__":
+    print("starting the maze")
     win = Window(500,500)
-    point_1 = Point(0,0)
-    point_2 = Point(500,500)
-    line=Line(point_1,point_2)
-    win.draw_line(line, "red")
-    win.wait_for_close()
+    print("Built the window")
+    maze = Maze(x1=0,y1=0,cell_size_x=10,cell_size_y=10,row_count=50,col_count=50,win=win)
+    print(maze._cells)
+    maze._draw_cells()
--- source/maze.py
+++ source/maze.py
@@ -1,6 +1,7 @@
 from .window import Window
 from .cell import Cell
 from dataclasses import dataclass
+import time
 
 @dataclass
 class Maze:
@@ -11,14 +12,26 @@ class Maze:
     cell_size_x: int
     cell_size_y: int
     win: Window
-    _cells: list[list[Cell]]
 
     def __post_init__(self) -> None:
         self._create_cells()
 
     def _create_cells(self) -> None:
-        self._cells = [[ Cell(x1+(j*self.cell_size_x),
-                     y1+(j*self.cell_size_y),
-                     x1+((j+1)*self.cell_size_x),
-                     y1+((j+1)*self.cell_size_y),
-                     self.win) for j in range(col_count) ] for i in range(row_count)]
+        self._cells = [[ Cell(self.x1+(j*self.cell_size_x),
+                     self.y1+(j*self.cell_size_y),
+                     self.x1+((j+1)*self.cell_size_x),
+                     self.y1+((j+1)*self.cell_size_y),
+                     self.win) for j in range(self.col_count) ] for i in range(self.row_count)]
+
+    def _draw_cells(self, i: int = 0, j: int = 0) -> None:
+        for row in self._cells:
+            print(row)
+            for cell in row:
+                cell.draw()
+                self._animate()
+
+
+    def _animate(self) -> None:
+        while True:
+            self.win.redraw()
+            time.sleep(0.05)
